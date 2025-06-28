b1b948860593dd6f65115175c7f0e443fed84f67
Samuel Foli Sosu <samuel.foli.sosu@gmail.com>
2025-06-28 11:41:04 +0000

    Modified task_3.sql file

--- task_3.sql
+++ task_3.sql
@@ -6,3 +6,9 @@ Write a script that list all the tables in alx_book_store in your MySQL server.
 The name of the file should be task_3.sql
 The database name will be passed as argument of mysql command
 */
+
+-- task_3.sql
+
+-- This script lists all tables in the database specified by the MySQL command.
+-- For example, to run this: mysql -u your_user -p alx_book_store < task_3.sql
+SHOW TABLES;
