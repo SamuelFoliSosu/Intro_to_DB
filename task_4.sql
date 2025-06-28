68b1c6c8ba0b0b29b3daf1c0d31856feb70c1a18
Samuel Foli Sosu <samuel.foli.sosu@gmail.com>
2025-06-28 13:00:06 +0000

    Modified task_4.sql file

--- task_4.sql
+++ task_4.sql
@@ -4,7 +4,7 @@ mandatory
 Write a script that prints the full description of the table books from the database alx_book_store in your MySQL server.
 
 The database name will be passed as an argument of the mysql command
-You are not allowed to use the DESCRIBE or EXPLAIN statements
+You are not allowed to use the  or  statements
 The name of the file should be task_4.sql
 All SQL keywords should be in uppercase
 */
@@ -13,6 +13,18 @@ All SQL keywords should be in uppercase
 -- Use the alx_book_store database
 USE alx_book_store;
 
--- This command shows the CREATE TABLE statement for the 'Books' table.
--- It provides a comprehensive definition of the table, including all columns and their properties.
-SHOW CREATE TABLE Books;
+-- Retrieve full column details for the 'Books' table from INFORMATION_SCHEMA.COLUMNS.
+-- by selecting relevant metadata about each column.
+SELECT
+    COLUMN_NAME AS Field,
+    COLUMN_TYPE AS Type,
+    IS_NULLABLE AS Null,
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
+    EXTRA AS Extra
+FROM
+    INFORMATION_SCHEMA.COLUMNS
+WHERE
+    TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books'
+ORDER BY
+    ORDINAL_POSITION;
