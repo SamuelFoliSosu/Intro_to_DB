05bc5b58b3a64397a84a8ab2926533f3b8780bb9
Samuel Foli Sosu <samuel.foli.sosu@gmail.com>
2025-06-28 11:51:41 +0000

    Modified task_6.sql file

--- task_6.sql
+++ task_6.sql
@@ -24,3 +24,13 @@ The database name will be passed as an argument of the mysql command
 
 The name of the file should be task_6.sql
 */
+-- task_6.sql
+
+-- Use the alx_book_store database
+USE alx_book_store;
+
+-- Insert multiple rows into the Customers table
+INSERT INTO customer (customer_id, customer_name, email, address) VALUES
+(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
